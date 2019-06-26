51fe5b95ff35e98f32b5b3d73d8eca57e1c5cb1f
patric-lynn <358812977@163.com>
2019-06-26 20:29:30 +0800

    增加候选主题字符串长度统计的功能： 对应SemanticIndex下的LengthCalculation包下的代码与文件。

--- SemanticIndex/Semantic.py
+++ SemanticIndex/Semantic.py
@@ -1,5 +1,20 @@
-import requests
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('UTF-8')
 
-def showlength():
-    with open('count.txt', 'w') as f:
-        for i in f:
+def showlength(value):
+    length = (len(value))
+    utf8_length = len(value.encode('gbk'))
+    length = str((utf8_length - length)/2 + length)
+    return length
+
+with open('count.txt', 'w') as count:
+    with open('kwd.txt','r') as kwd:
+        content = kwd.readlines()
+        for i in content:
+            word = i.strip()
+            length2word = showlength(word)
+            count.write(length2word + '\n')
+            print "词语" + i + "的长度为" + length2word
+print ("计算结束")
